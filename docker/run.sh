46a16fdcc379d65f177d86d78c43858dfb94ee74
Raysmond <jiankunlei@126.com>
2015-09-26 17:07:44 +0800

    Add meta title

--- docker/run.sh
+++
@@ -1,4 +0,0 @@
-# docker run --name mysql -e MYSQL_ROOT_PASSWORD=root -d mysql:latest
-
-docker run -p 8080:8080 --link mysql:mysql \
--v /Users/Raysmond/Projects/Lab/SeGA/TaskViewer/build/libs:/opt/jetty/webapps niaquinto/jetty
--- src/main/java/com/raysmond/blog/support/web/ViewHelper.java
+++ src/main/java/com/raysmond/blog/support/web/ViewHelper.java
@@ -4,11 +4,16 @@ package com.raysmond.blog.support.web;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * Created by Raysmond on 9/26/15.
  */
 @Service
 public class ViewHelper {
+
+    private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("MMMM dd, yyyy");
 
     private long startTime;
 
@@ -22,6 +27,10 @@ public class ViewHelper {
 
     public void setStartTime(long startTime) {
         this.startTime = startTime;
+    }
+
+    public String getFormattedDate(Date date){
+        return date == null ? "" : DATE_FORMAT.format(date);
     }
 
 }
