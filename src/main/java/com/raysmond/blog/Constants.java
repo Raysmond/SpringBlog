55c0876858855b786fb1f435655e7c4b3c29193f
Raysmond <i@raysmond.com>
2018-01-19 12:36:35 +0800

    spring profiles reorganized

--- src/main/java/com/raysmond/blog/Constants.java
+++ src/main/java/com/raysmond/blog/Constants.java
@@ -5,11 +5,11 @@ package com.raysmond.blog;
  */
 public final class Constants {
 
-    public static final String ENV_PRODUCTION = "production";
+    public static final String ENV_PRODUCTION = "prod";
 
-    public static final String ENV_DEVELOPMENT = "development";
+    public static final String ENV_DEVELOPMENT = "dev";
 
-    public static final String DEFAULT_ADMIN_EMAIL = "admin@admin.com";
+    public static final String DEFAULT_ADMIN_EMAIL = "admin";
 
     public static final String DEFAULT_ADMIN_PASSWORD = "admin";
 
--- src/main/java/com/raysmond/blog/SpringBlogApplication.java
+++ src/main/java/com/raysmond/blog/SpringBlogApplication.java
@@ -1,14 +1,18 @@
 package com.raysmond.blog;
 
+import com.google.common.collect.ImmutableMap;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+
 
 @SpringBootApplication
 @EnableCaching
 public class SpringBlogApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBlogApplication.class, args);
+        SpringApplication app = new SpringApplication(SpringBlogApplication.class);
+        app.setDefaultProperties(ImmutableMap.of("spring.profiles.default", Constants.ENV_DEVELOPMENT));
+        app.run(args);
     }
 }
